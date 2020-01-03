52d8fa4d8b0a1be1bcb88665cbc2a6e2726032ce
Atikur Zaman Pallob <atikurzamanpallob@gmail.com>
2020-01-03 19:57:17 +0600

    Updates

---
+++ precache-manifest.1d0f99624b2af749bb15b98d5d5b16cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4037515f1263c3920c68cf179951e91",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "477d013d4088d48ac86e",
+    "url": "/profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "efb6d4e7cc55fb3e5061",
+    "url": "/profile/static/js/2.8815829c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.8815829c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "477d013d4088d48ac86e",
+    "url": "/profile/static/js/main.5855bc4e.chunk.js"
+  },
+  {
+    "revision": "8b2d8476375ca005a746",
+    "url": "/profile/static/js/runtime-main.ba0cf1c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
